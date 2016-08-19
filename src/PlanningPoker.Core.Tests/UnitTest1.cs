549abbed21db2027d7a9f4403b820f1a76f99e85
charri <git@a.charri.ch>
2016-08-19 11:32:52 +0200

    init

---
+++ src/PlanningPoker.Core.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlanningPoker.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
